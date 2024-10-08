67de1f5a2bc33e0caabc0cfeaf51ff6afbab3ee4
Your Name <you@example.com>
2024-10-08 22:04:20 +0530

    Add after reset password comfirmation

--- routes/admins.js
+++ routes/admins.js
@@ -1,28 +1,15 @@
-// let express = require('express');
-// let router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-// let adminController = require("../Controller/Admin")
+let adminController = require("../Controller/Admin")
 
-
-// router.post('/create', adminController.adminCreate);
-// router.post('/login', adminController.adminLogin);
-// router.get('/all', adminController.allAdmin);
-
-
-
-// module.exports = router;
-
-// router/admin.js
-const express = require('express');
-const router = express.Router();
-const adminController = require("../Controller/Admin");
 
 router.post('/create', adminController.adminCreate);
 router.post('/login', adminController.adminLogin);
 router.get('/all', adminController.allAdmin);
-
-// Password Reset with New Password and Confirm Password
 router.post('/resetPasswordWithConfirmation', adminController.resetPasswordWithConfirmation);
 
 module.exports = router;
+
+
 
