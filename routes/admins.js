fe688d8ebd761323c69586b1b92f5ac5b33e6138
Your Name <you@example.com>
2024-10-09 08:59:24 +0530

    Add after update

--- routes/admins.js
+++ routes/admins.js
@@ -7,12 +7,14 @@ let adminController = require("../Controller/Admin")
 router.post('/create', adminController.adminCreate);
 router.post('/login', adminController.adminLogin);
 router.get('/all', adminController.allAdmin);
+
 // Request OTP for password reset
 router.post('/requestOtp', adminController.requestOtp);
-
 // Verify OTP and reset password
 router.post('/verifyOtpAndResetPassword', adminController.verifyOtpAndResetPassword);
-// router.post('/resetPasswordWithConfirmation', adminController.resetPasswordWithConfirmation);
+
+
+router.post('/resetPasswordWithConfirmation', adminController.resetPasswordWithConfirmation);
 
 module.exports = router;
 
